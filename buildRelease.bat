44cc4e77b53c37d3c594db863dc858046aaf7f41
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-11-29 08:47:37 -0500

    Rebuilt for 1.2.1 Added version info into assembly file Added scripts for local deply and release building

---
+++ buildRelease.bat
@@ -0,0 +1,38 @@
+﻿@echo off
+
+copy /Y "bin\Release\NavBallDockingAlignmentIndicatorCE.dll" "GameData\NavBallDockingAlignmentIndicatorCE\Plugins"
+copy /Y NavBallDockingAlignmentIndicatorCE.version GameData\NavBallDockingAlignmentIndicatorCE
+copy /Y ..\MiniAVC.dll GameData\NavBallDockingAlignmentIndicatorCE
+
+set DEFHOMEDRIVE=d:
+set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
+set HOMEDIR=
+set HOMEDRIVE=%CD:~0,2%
+
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+echo Default homedir: %DEFHOMEDIR%
+
+rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
+
+if "%HOMEDIR%" == "" (
+set HOMEDIR=%DEFHOMEDIR%
+) 
+echo %HOMEDIR%
+
+SET _test=%HOMEDIR:~1,1%
+if "%_test%" == ":" (
+set HOMEDRIVE=%HOMEDIR:~0,2%
+)
+
+
+type NavBallDockingAlignmentIndicatorCE.version
+set /p VERSION= "Enter version: "
+
+
+copy /y /s license.md GameData\NavBallDockingAlignmentIndicatorCE
+
+
+set FILE="%RELEASEDIR%\NavBallDockingAlignmentIndicatorCE-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% GameData
---
+++ deploy.bat
@@ -0,0 +1,13 @@
+﻿
+set H=R:\KSP_1.1.4_dev
+echo %H%
+
+
+copy /Y "bin\Debug\NavBallDockingAlignmentIndicatorCE.dll" "GameData\NavBallDockingAlignmentIndicatorCE\Plugins"
+copy /Y NavBallDockingAlignmentIndicatorCE.version GameData\NavBallDockingAlignmentIndicatorCE
+
+cd GameData
+mkdir "%H%\GameData\NavBallDockingAlignmentIndicatorCE"
+xcopy /y /s NavBallDockingAlignmentIndicatorCE "%H%\GameData\NavBallDockingAlignmentIndicatorCE"
+
+pause
