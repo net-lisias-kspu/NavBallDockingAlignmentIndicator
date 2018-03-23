6626746d8321c07443103950d342b750b998719d
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-03-23 17:34:34 -0400

    Updated fr 1.4.1

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,28 +1,50 @@
 ﻿
 @echo off
 
+rem Put the following text into the Post-build event command line:
+rem without the "rem":
+
+rem start /D D:\Users\jbb\github\IFI-Life-Support /WAIT deploy.bat  $(TargetDir) $(TargetFileName)
+rem 
+rem if $(ConfigurationName) == Release (
+rem 
+rem start /D D:\Users\jbb\github\IFI-Life-Support /WAIT buildRelease.bat $(TargetDir) $(TargetFileName)
+rem 
+rem )
+
+
 rem Set variables here
 
+rem H is the destination game folder
+rem GAMEDIR is the name of the mod folder (usually the mod name)
+rem GAMEDATA is the name of the local GameData
+rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
+rem    but not always
+rem LICENSE is the license file
+rem README is the readme file
+
 set GAMEDIR=NavBallDockingAlignmentIndicatorCE
-set LICENSE=License.md
-set README=ReadMe.md
+set GAMEDATA="GameData\"
+set VERSIONFILE=%GAMEDIR%.version
+set LICENSE=license.md
+set README=README.md
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
 
 rem Copy files to GameData locations
 
-copy /Y "%1%2" "GameData\%GAMEDIR%\Plugins"
-copy /Y %GAMEDIR%.version GameData\%GAMEDIR%
-copy /Y ..\MiniAVC.dll GameData\%GAMEDIR%
+copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
+copy /Y ..\MiniAVC.dll %GAMEDATA%\%GAMEDIR%
 
-if "%LICENSE%" NEQ "" copy /y  %LICENSE% GameData\%GAMEDIR%
-if "%README%" NEQ "" copy /Y %README% GameData\%GAMEDIR%
+if "%LICENSE%" NEQ "" copy /y  %LICENSE% %GAMEDATA%\%GAMEDIR%
+if "%README%" NEQ "" copy /Y %README% %GAMEDATA%\%GAMEDIR%
 
 rem Get Version info
 
-copy %GAMEDIR%.version a.version
-set VERSIONFILE=a.version
+copy %VERSIONFILE% tmp.version
+set VERSIONFILE=tmp.version
 rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
 c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
 set /P major=<tmpfile
@@ -36,7 +58,7 @@ set /P patch=<tmpfile
 c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
 set /P build=<tmpfile
 del tmpfile
-del a.version
+del tmp.version
 set VERSION=%major%.%minor%.%patch%
 if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
 
@@ -44,6 +66,7 @@ echo Version:  %VERSION%
 
 
 rem Build the zip FILE
+cd %GAMEDATA%\..
 
 set FILE="%RELEASEDIR%\%GAMEDIR%-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,20 @@
 ﻿
-set H=R:\KSP_1.3.1_dev
-echo %H%
+@echo off
 
+rem H is the destination game folder
+rem GAMEDIR is the name of the mod folder (usually the mod name)
+rem GAMEDATA is the name of the local GameData
+rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
+rem    but not always
 
-copy /Y "bin\Debug\NavBallDockingAlignmentIndicatorCE.dll" "GameData\NavBallDockingAlignmentIndicatorCE\Plugins"
-copy /Y NavBallDockingAlignmentIndicatorCE.version GameData\NavBallDockingAlignmentIndicatorCE
+set H=R:\KSP_1.4.1_dev
+set GAMEDIR=NavBallDockingAlignmentIndicatorCE
+set GAMEDATA="GameData\"
+set VERSIONFILE=%GAMEDIR%.version
 
-cd GameData
+copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
-xcopy /y /s /i NavBallDockingAlignmentIndicatorCE "%H%\GameData\NavBallDockingAlignmentIndicatorCE"
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+
+pause
